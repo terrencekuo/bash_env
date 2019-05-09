26a12ac7928a508916501e7c1be7c7ed3c2eb2c4
terrencekuo <kuuott@gmail.com>
2019-05-08 21:29:14 -0700

    setup: automatically setup files & plugins

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,7 @@
+# copy the vimrc and tmux.conf files to the home directory
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+
 # install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
